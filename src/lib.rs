e365cd03cede5b8a264f41f638400d60a6d2b547
jagadeeshftw <jagadeesh26062002@gmail.com>
2026-07-11 13:43:40 +0530

    chore: bump crate version to 0.6.0

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@ impl AnchornetContract {
 
     /// Returns the contract interface version.
     pub fn version() -> u32 {
-        5
+        6
     }
 
     /// Returns `true` if the contract has been initialized.
